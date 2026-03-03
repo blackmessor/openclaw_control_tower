f8abc36b41cd6a7ea3b24c6f99783ac7cdb74528
Nicolas Godefroy <nicolasgodefroy@blackmessor.com>
2026-03-03 15:51:03 +0100

    reset: abandon Tauri, new stack Bun+Elysia+HTMX+Alpine

--- src-tauri/src/main.rs
+++
@@ -1,6 +0,0 @@
-// Prevents additional console window on Windows in release, DO NOT REMOVE!!
-#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
-
-fn main() {
-    openclaw_control_tower_lib::run()
-}
--- src/routes/health/+page.svelte
+++
@@ -1,9 +0,0 @@
-&lt;script lang=&quot;ts&quot;&gt;
-  import HealthCard from &apos;$lib/components/HealthCard.svelte&apos;;
-&lt;/script&gt;
-
-&lt;div class=&quot;grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 gap-6&quot;&gt;
-  &lt;HealthCard title=&quot;Status&quot; loadFn=&quot;openclaw_status&quot; icon=&quot;❤️&quot; /&gt;
-  &lt;HealthCard title=&quot;Agents&quot; loadFn=&quot;agents_list&quot; icon=&quot;🤖&quot; /&gt;
-  &lt;HealthCard title=&quot;Sessions&quot; loadFn=&quot;sessions_list&quot; icon=&quot;📜&quot; /&gt;
-&lt;/div&gt;
